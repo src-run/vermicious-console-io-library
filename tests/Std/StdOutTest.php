ba2fe6551e4a8b2cc32aaed860e6630ee1334678
Rob Frawley 2nd <rmf@src.run>
2016-06-05 02:17:42 -0400

    fixed file-level doc-block project name

--- tests/Std/StdOutTest.php
+++ tests/Std/StdOutTest.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the `src-run/augustus-environment-library` project.
+ * This file is part of the `vermicious-console-io-library` project.
  *
  * (c) Rob Frawley 2nd <rmf@src.run>
  *
